112295874ef52359dfc635b9c35980429257104a
Maksim K <6606745@gmail.com>
2024-10-22 03:19:18 +0300

    3.3 Mobile:js, pixel perfect

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -23,3 +23,45 @@ sortSelect.addEventListener('click', () => {
 sortSelect.addEventListener('blur', () => {
   sortCatalog.classList.remove('active');
 });
+
+
+//делаем слайдер
+const nextBtn = document.querySelector('.promo__slider-arrow--next');
+const prevBtn = document.querySelector('.promo__slider-arrow--prev');
+const slides = document.querySelectorAll('.promo__container');
+let changeSlide = 0;
+
+nextBtn.addEventListener('click', () => {
+
+  slides.forEach((slide, index) => {
+    if (slide.classList.contains('promo__container-show') === true) {
+      changeSlide = index + 1;
+      slide.classList.remove('promo__container-show');
+    }
+  });
+
+  if (changeSlide < slides.length) {
+    slides[changeSlide].classList.add('promo__container-show');
+  } else {
+    changeSlide = 0;
+    slides[changeSlide].classList.add('promo__container-show');
+  }
+});
+
+prevBtn.addEventListener('click', () => {
+
+  slides.forEach((slide, index) => {
+    if (slide.classList.contains('promo__container-show') === true) {
+      changeSlide = index - 1;
+      slide.classList.remove('promo__container-show');
+    }
+  });
+
+  if (changeSlide < slides.length && changeSlide > -1) {
+    slides[changeSlide].classList.add('promo__container-show');
+  } else {
+    changeSlide = slides.length - 1;
+    slides[changeSlide].classList.add('promo__container-show');
+  }
+});
+
